e23bca46cd71042ea5edf6886ad76a7c6f2c8cda
ino <littletasteofheaven@protonmail.com>
2022-06-30 07:06:16 +0600

    Add facebook and instagram links

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,7 @@
 import {Link, Stack, Typography} from "@mui/material";
 import {useTheme} from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import {FacebookRounded, Instagram} from "@mui/icons-material";
 
 export function Contact() {
   const theme = useTheme()
@@ -12,24 +13,21 @@ export function Contact() {
       direction="column"
       alignItems="center" textAlign="center">
       <Typography
-        variant={screenTablet? "h2": "h4"}
+        variant={screenTablet ? "h2" : "h4"}
         gutterBottom
         sx={{
           marginTop: "1.5rem"
         }}>
-        For any query
+        Find us on
       </Typography>
-      <Link
-        underline="hover"
-        variant={screenTablet? "h2": "h4"}
-        sx={{
-          cursor: "pointer"
-        }}
-        onClick={
-          () => window.location.href = "mailto:littledarkrain@gmail.com"
-        }>
-        Shoot us a mail!
-      </Link>
+      <Stack>
+        <Link href="https://www.facebook.com/B-Motors-107782258650766">
+          <FacebookRounded fontSize="large"/>
+        </Link>
+        <Link href="https://www.instagram.com/bmotors4u/">
+          <Instagram fontSize="large"/>
+        </Link>
+      </Stack>
     </Stack>
   )
 }
