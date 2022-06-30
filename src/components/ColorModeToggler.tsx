1d2ae862141a9d39be82b84e334ed1473aacb3f3
ino <littletasteofheaven@protonmail.com>
2022-06-30 07:05:57 +0600

    Use help text

--- src/components/ColorModeToggler.tsx
+++ src/components/ColorModeToggler.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {IconButton} from "@mui/material";
+import {IconButton, Stack, Typography} from "@mui/material";
 import Brightness7 from "@mui/icons-material/Brightness7";
 import Brightness4 from "@mui/icons-material/Brightness4";
 import {propsT} from "./Navbar";
@@ -14,21 +14,29 @@ export function ColorModeToggler(props: propsT) {
   const screenTablet = useMediaQuery(theme.breakpoints.up('sm'))
 
   return (
-    <IconButton
-      size="small"
-      onClick={() => setColorMode(colorMode == "light" ? "dark" : "light")}
-      color="info">
-      {colorMode === 'dark' ?
-        <Brightness7
-          sx={{
-            width: () => screenTablet ? "3rem" : "2rem",
-            height: () => screenTablet ? "3rem" : "2rem"
-          }}/> :
-        <Brightness4
-          sx={{
-            width: () => screenTablet ? "3rem" : "2rem",
-            height: () => screenTablet ? "3rem" : "2rem"
-          }}/>}
-    </IconButton>
+    <Stack sx={{
+      flexDirection: "row",
+      alignItems: "center",
+      cursor: "pointer",
+    }} onClick={() => setColorMode(colorMode == "light" ? "dark" : "light")}>
+      <Typography>
+        {colorMode}
+      </Typography>
+      <IconButton
+        size="small"
+        color="info">
+        {colorMode === 'dark' ?
+          <Brightness7
+            sx={{
+              width: () => screenTablet ? "3rem" : "1.5rem",
+              height: () => screenTablet ? "3rem" : "1.5rem"
+            }}/> :
+          <Brightness4
+            sx={{
+              width: () => screenTablet ? "3rem" : "1.5rem",
+              height: () => screenTablet ? "3rem" : "1.5rem"
+            }}/>}
+      </IconButton>
+    </Stack>
   );
 }
